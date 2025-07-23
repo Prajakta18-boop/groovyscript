94b2623f93749744aca96cc98c0cb6e78986db79
prajakta <prajaktaks2004@gmail.com>
2025-07-23 15:52:09 +0530

    pipeline

--- practice.groovy
+++ practice.groovy
@@ -5,6 +5,18 @@ pipeline{
         steps{ 
             git branch: 'main', url: 'https://github.com/Prajakta18-boop/newgroovy.git'
             }
+            
+    }
+    stage('deploying code') {
+        steps{
+            sh 'mv /var/lib/jenkins/workspace/agentA/index.html /var/www/html/jewels'
+        }
+    }   
+    stage ('restarting nginx'){
+        steps{
+           sh 'sudo systemctl restart nginx'
+        }
     }
    }
+
 }
