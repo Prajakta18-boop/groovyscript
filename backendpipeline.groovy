949f9b8b481de371489174fb6e134e982eea6a27
prajakta <prajaktaks2004@gmail.com>
2025-07-29 15:50:47 +0530

    first commit

--- backendpipeline.groovy
+++ backendpipeline.groovy
@@ -3,7 +3,7 @@ pipeline{
    stages{ 
     stage('clone code') {
         steps{ 
-            git branch: 'main', url: 'https://github.com/Prajakta18-boop/newgroovy.git'
+            git branch: 'main', url: 'https://github.com/Prajakta18-boop/backendcode.git'
             }
             
         }
