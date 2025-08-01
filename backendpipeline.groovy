772568194c6d614d0a1c49935781ab8155b7eccd
prajakta <prajaktaks2004@gmail.com>
2025-08-01 16:01:18 +0530

    first commit

--- backendpipeline.groovy
+++ backendpipeline.groovy
@@ -1,34 +1,38 @@
-pipeline{
-    agent any 
+pipeline {
+    agent any
     tools {
         maven 'maven3.9.8'
     }
-     
-   
-    stage('clone code') {
-        steps{ 
-            git branch: 'main', url: 'https://github.com/Prajakta18-boop/backendcode.git'
+
+    stages {
+        stage('Clone Code') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Prajakta18-boop/backendcode.git'
             }
-            
         }
-        stage('build'){
-            steps{
+
+        stage('Build') {
+            steps {
                 sh 'mvn clean package'
             }
-
         }
-        stage('sending artifact to tomcat server'){
-            steps{
-                sh 'scp target/*.war azureuser@4.186.26.126:/home/azureuser/'
+
+        stage('Send Artifact to Tomcat Server') {
+            steps {
+                sshagent(['tomcat-server']) {
+                    sh 'scp -o StrictHostKeyChecking=no target/*.war azureuser@4.186.26.126:/home/azureuser/'
+                }
             }
-
         }
-        stage('deploying artifact'){
-            steps{
-                sshagent(['tomcat-server']){
-                    sh 'ssh -o StrictHostKeyChecking=no azureuser@4.186.26.126 "sudo mv /home/azureuser/*.war /opt/tomcat/webapps"'
+
+        stage('Deploy Artifact') {
+            steps {
+                sshagent(['tomcat-server']) {
+                    sh 'ssh -o StrictHostKeyChecking=no azureuser@4.186.26.126 "sudo mv /home/azureuser/*.war /opt/tomcat/webapps/"'
                 }
             }
         }
     }
+}
+
    
